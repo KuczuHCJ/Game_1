341e5977da327a270ea06a435139ff4b27685e66
Kacper Kuczowicz <kacper.kuczowicz@outlook.com>
2023-10-22 20:28:42 +0200

    dorobiona możliwość gry z kompterem

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace Kamień_papier_nożyce;
 
 class Program
 {
+
+    
     static void Main(string[] args)
         //Początek gry instrukcja, przywitanie 
     {
@@ -11,10 +14,55 @@ class Program
         Console.WriteLine("Hej witaj w mojej grze, polega ona na wybraniu:\n1-Kamień\n2-Nożyce\n3-Papier");
         Console.WriteLine("Zasady są proste kamien wygrywa z nożycami, nożyce z papierem a papier z kamieniem");
         Console.WriteLine("potrzebujesz kolegi który zaga z toba");
-        Console.ResetColor();
+        koniec_koloru();
 
-        //W planie jest zrobienie później jeszcze wersji w której można będzie grć z komputerem
-        //definicja zmiennych dla punktów zdobytych ogółem
+        //wprowadzenie zmiennej odpowiadającej grze z komputerem bazowo true
+        bool gra_z_graczem = true;
+        //wprowadzenie zmiennej b, zostanie zastąpiona w pierwszym obiegu pętli gra przez gracza lub komputer
+        //pamiętać że b mozna zastąpić wartość w kodzie ale nie mozna jej zadeklarować dwa razy jakieś 1,5h szukałem błędu 
+        //po ponownym zadeklarowaniu b w insstrukcjach warunkowych gry z komputerem 
+
+        int b = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Czy chcesz zagrać z innym graaczem? - wybierz 1, czy komputerem? wybierz 2");
+            String gvsk = Console.ReadLine();
+            if (int.TryParse(gvsk, out int gracz))
+            {
+                if (gracz != 1 && gracz != 2)
+                {
+                    czerwony_kolor_tekstu();
+                    Console.WriteLine("wybrano nie właściwą liczbę wybierz liczbę z przedziału 1-2");
+                    koniec_koloru();
+                    continue;
+                }
+                else if(gracz == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("wybrano grę z innym graczem");
+                    koniec_koloru();
+                }
+                else if(gracz == 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("wybrano grę z komputerem, powodzenia!");
+                    koniec_koloru();
+                    gra_z_graczem = false;
+                }
+                break;
+            }
+
+            else
+            {
+                czerwony_kolor_tekstu();
+                Console.WriteLine("prawdopodobnie wpisano tekst zamiast liczby");
+                koniec_koloru();
+                continue;
+
+            }
+        }
+
         int punkty_całości_1 = 0;
         int punkty_całości_2 = 0;
         //Instrukcja go to w razie chęci kontynuacji gry 
@@ -28,7 +76,7 @@ class Program
         
         while (true)
         {
-            //spytać o to jak by to przeparsowac metodą try pare żeby nie wywalało mi programu 
+            
             //Licznik punktów wyświetlany po 1 obiegu pętli 
             if (licznik > 0)
             {
@@ -54,10 +102,10 @@ class Program
                 punkty_całości_2++;
                 break;
             }
-            Console.ResetColor();
+            koniec_koloru();
 
             //wpisuwanie przez graczy wartości
-                        Console.WriteLine("gracz pierwszy:");
+            Console.WriteLine("gracz pierwszy:");
             string a_to_parse = (Console.ReadLine());
             if (int.TryParse(a_to_parse, out int a))
             {
@@ -65,34 +113,50 @@ class Program
             }
             else
             {
-           
-                Console.ForegroundColor = ConsoleColor.Red;
+
+                czerwony_kolor_tekstu();
                 Console.WriteLine("gracz 1 - podaj wartość liczbową, reset rundy");
-                Console.ResetColor();
+                koniec_koloru();
                 continue;
             }
 
-            Console.WriteLine("gracz drugi:");
-            string b_to_parse = (Console.ReadLine());
-            if (int.TryParse(b_to_parse, out int b))
+            if (gra_z_graczem == true)
+                  
+            {
+                
+                Console.WriteLine("gracz drugi:");
+                string b_to_parse = (Console.ReadLine());
+             
+                if (int.TryParse(b_to_parse, out  b))
+                {
+                    
+                }
+                else
+                {
+                    czerwony_kolor_tekstu();
+                    Console.WriteLine("gracz 2 - podaj wartość liczbową, reset rundy!");
+                    koniec_koloru();
+                    continue;
+                }
+               
+            }
+
+            if  (gra_z_graczem == false)
             {
 
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("gracz 2 - podaj wartość liczbową, reset rundy!");
-                Console.ResetColor();
-                continue;
+                Random liczby = new Random();
+                b = liczby.Next(1, 4);
+             
+                Console.WriteLine(b);
             }
 
             //Warunek dla sytuacji w której gracze wybrali liczby z poza przedziału
-
+            
             if (a > 3 || a < 1 || b > 3 || b < 1)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("któryś z graczy nie wybrał liczby z przedziału 1-3");
-                Console.ResetColor();
+                koniec_koloru();
                 nowa_linia();
                 continue;
             }
@@ -104,7 +168,7 @@ class Program
             //Warunki remisu 
             if (a == b)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
+                czerwony_kolor_tekstu();
                 Console.WriteLine("Remis brak puktów");
                 Console.ResetColor();
                 nowa_linia();
@@ -118,7 +182,7 @@ class Program
             {
                 Console.WriteLine("gracz pierwszy wygrywa");
                 punkty_1++;
-                Console.ResetColor();
+                koniec_koloru();
                 nowa_linia();
                 continue;
             }
@@ -127,7 +191,7 @@ class Program
             {
                 Console.WriteLine("gracz drugi wygrywa");
                 punkty_2++;
-                Console.ResetColor();
+                koniec_koloru();
                 nowa_linia();
                 continue;
             }
@@ -150,9 +214,9 @@ class Program
     
         if (1 != Tak_nie && 2 != Tak_nie)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
+            czerwony_kolor_tekstu();
             Console.WriteLine("wybrano nie prawidłową watość!!!");
-            Console.ResetColor();
+            koniec_koloru();
             goto wybór;
         }
         //Go to do pętli while jako gry 
@@ -160,7 +224,7 @@ class Program
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("zaczynamy kolejną grę");
-            Console.ResetColor();
+            koniec_koloru();
             goto Gra;
         }
         //zakończenie gry 
@@ -203,4 +267,13 @@ class Program
         Console.WriteLine("\n");
     }
 
+    static void czerwony_kolor_tekstu()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+    }
+
+    static void koniec_koloru()
+    {
+        Console.ResetColor();
+    }
 }
