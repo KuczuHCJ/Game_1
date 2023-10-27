8d74c5bb7967d3048d5af5ca4d1dffa4cb3c9639
Kacper Kuczowicz <kacper.kuczowicz@outlook.com>
2023-10-27 14:21:50 +0200

    poprawiono instrukcje blędnych wyborów

--- Program.cs
+++ Program.cs
@@ -12,35 +12,41 @@ class Program
 
 
     static void Main(string[] args)
-    
-    //Początek gry instrukcja, przywitanie określenie nazwy konsoli 
     {
-        Console.Title = "kamień / papier / nożyce";
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine("Hej witaj w mojej grze, polega ona na wybraniu:\n1-Kamień\n2-Nożyce\n3-Papier");
-        Console.WriteLine("Zasady są proste kamien wygrywa z nożycami, nożyce z papierem a papier z kamieniem");
-        Console.WriteLine("możesz w grę zagrać sam z komputerem lub z inną osobą.");
-        Koniec_koloru();
-
-        //wprowadzenie zmiennej odpowiadającej grze z komputerem bazowo true
+        Start();
+        /*
+        formula start zawiera instruckje gry wraz 
+        z przywitaniem oraz dwima innymi metodami które 
+        mają na celu nadać tekstowi odpowiednie formatowanie kolorystyczne 
+        */
         bool gra_z_graczem = true;
-        //wprowadzenie zmiennej b, zostanie zastąpiona w pierwszym obiegu pętli gra przez gracza lub komputer
-        //pamiętać że b mozna zastąpić wartość w kodzie ale nie mozna jej zadeklarować dwa razy jakieś 1,5h szukałem błędu 
-        //po ponownym zadeklarowaniu b w insstrukcjach warunkowych gry z komputerem 
-
-
+        //wprowadzenie zmiennej odpowiadającej grze z komputerem bazowo true
         int b = 0;
-
-        //tablice do przechowywania ruchów obu graczy
-        //długość tablic określa ich pojemność 
-        // i określa w późniejszym etapie dla którego elementu przypisać wybrany ruch
-
-        
+        //wprowadzenie zmiennej b, zostanie zastąpiona w pierwszym obiegu pętli gra przez gracza lub komputer
         List<int> lista_1 = new List<int>();
         List<int> lista_2 = new List<int>();
+        /*
+        tablice do przechowywania ruchów obu graczy
+        długość tablic określa ich pojemność 
+        i określa w późniejszym etapie dla którego elementu przypisać wybrany ruch
+        */
+        int punkty_całości_1 = 0;
+        int punkty_całości_2 = 0;
+        //zmienne do zliczania punktów w całości gry
+        int licznik = 0;
+        int punkty_1 = 0;
+        int punkty_2 = 0;
+        /*
+         zmienne do rozgrywki:
+        zmienne punkty zliczają punkty uzyskane w danej grze 
+        zmienna licznik ma za zdanie doliczanie w celu wyświetlania punktacji,
+        dopiero po rozpoczęciu drugiego obiegu pętlli
+        */
 
-        
 
+        /*
+          pętla warunkowa służaca do wyboru formy rozgrywki komputer / człowiek
+         */
 
         while (true)
         {
@@ -82,23 +88,24 @@ class Program
             }
         }
 
-        int punkty_całości_1 = 0;
-        int punkty_całości_2 = 0;
-       
+ 
+
     //Instrukcja go to w razie chęci kontynuacji gry 
+    // wyrażonej na kończu każej rozgrywki 
     Gra:
-        //zmienne licznik do obliczania przebiegu pętli
-        int licznik = 0;
-        int punkty_1 = 0;
-        int punkty_2 = 0;
-       
+    //restet punktów zdobytych w danej grze 
+    //oraz licznika zliczającego kolejną rundę
+        licznik = 0;
+        punkty_1 = 0;
+        punkty_2 = 0;
 
 
-        //Pętla jako sama gra 
+
+        //Gra w pętli while 
 
         while (true)
         {
-            
+
             //Licznik punktów wyświetlany po 1 obiegu pętli 
             if (licznik > 0)
             {
@@ -108,7 +115,7 @@ class Program
                 Nowa_linia();
             }
 
-            //Warunki wygranej
+            //Warunki wygranej w całej rozgrywce przez obu użtkowników / użytkownika i komputer 
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             if (punkty_1 >= 3)
             {
@@ -126,62 +133,81 @@ class Program
             }
             Koniec_koloru();
 
-            //wpisuwanie przez graczy wartości
+            /* na początku każdej rozgrywwki rsetuje b. 
+            następnie gracze wpisując wartości liczbowe które odpowiadają 
+            typowi int na wypadek nie wpisana typu int przez graczy komputer informuje o tym 
+            i przenosi do ponownego wyboru 
+            na wypadek wpisana liczby z poza zakresu 1-3 komputer przenosi gracza do pononego 
+            wpisu danego gracza, bez konieczności wpisaywania przez obu nowych wartości 
+            */
+
+            b = 0;
+            do_a:
             Console.WriteLine("gracz pierwszy:");
             string a_to_parse = (Console.ReadLine());
-            if (int.TryParse(a_to_parse, out int a))
+            if (int.TryParse(a_to_parse, out int a) && b != 0)
             {
-
+                goto po_a;
             }
-            else
+            else if (a !is int)
             {
 
                 Czerwony_kolor_tekstu();
-                Console.WriteLine("gracz 1 - podaj wartość liczbową, reset rundy");
+                Console.WriteLine("gracz 1 - podaj wartość liczbową, reset rundy w zakresie 1-3");
                 Koniec_koloru();
                 continue;
             }
 
+        wybor_gracz_2:
             if (gra_z_graczem == true)
-                  
+            
             {
-                
                 Console.WriteLine("gracz drugi:");
                 string b_to_parse = (Console.ReadLine());
-             
-                if (int.TryParse(b_to_parse, out  b))
+                if (int.TryParse(b_to_parse, out b))
                 {
-                    
+
                 }
-                else
+                else if (b !is int)
                 {
                     Czerwony_kolor_tekstu();
-                    Console.WriteLine("gracz 2 - podaj wartość liczbową, reset rundy!");
+                    Console.WriteLine("gracz 2 - podaj wartość liczbową, w zakresie 1-3");
                     Koniec_koloru();
-                    continue;
+                    goto wybor_gracz_2;
                 }
-               
+                    
             }
 
-            if  (gra_z_graczem == false)
+            if (gra_z_graczem == false)
             {
 
                 Random liczby = new();
                 b = liczby.Next(1, 4);
-             
+
                 Console.WriteLine(b);
             }
 
             //Warunek dla sytuacji w której gracze wybrali liczby z poza przedziału
-            
-            if (a > 3 || a < 1 || b > 3 || b < 1)
+            po_a:
+
+            if (a > 3 || a < 1)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("któryś z graczy nie wybrał liczby z przedziału 1-3");
+                Console.WriteLine("Gracz pierwszy wybrał liczbę z poza przedziału 1-3");
                 Koniec_koloru();
                 Nowa_linia();
-                continue;
+                goto do_a;
             }
+            else if(b > 3 || b < 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Gracz drugi wybrał liczbę z poza przedziału 1-3");
+                Koniec_koloru();
+                Nowa_linia();
+                goto wybor_gracz_2;
+            }
+
+
             licznik++;
 
 
@@ -271,7 +297,7 @@ class Program
             Console.WriteLine("podaj liczbę");
             goto wybór;
         }
-        
+
         //kod podsumowywujący rozgrywkę
 
         if (punkty_całości_1 == punkty_całości_2)
@@ -294,7 +320,7 @@ class Program
         //wysłanie danych  z gry do innego pliku 
         for (int i = 0; i < lista_1.Count; i++)
         {
-            Console.WriteLine($"ruch gracza 1 = {lista_1[i]} ruch gracza 2 = {lista_2[i]} w rundzie{i+1}");
+            Console.WriteLine($"ruch gracza 1 = {lista_1[i]} ruch gracza 2 = {lista_2[i]} w rundzie{i + 1}");
         }
 
         //podpis 
@@ -321,5 +347,15 @@ class Program
         Console.ResetColor();
     }
 
- 
+    static void Start()
+    {
+        Console.Title = "kamień / papier / nożyce";
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Hej witaj w mojej grze, polega ona na wybraniu:\n1-Kamień\n2-Nożyce\n3-Papier");
+        Console.WriteLine("Zasady są proste kamien wygrywa z nożycami, nożyce z papierem a papier z kamieniem");
+        Console.WriteLine("możesz w grę zagrać sam z komputerem lub z inną osobą.");
+        Koniec_koloru();
+    }
+
+
 }
